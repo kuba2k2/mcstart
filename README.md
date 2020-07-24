9d09cf6241e3b482d8bc8bd24bd89ec9f9b4b5e3
kuba2k2 <kuba@szczodrzynski.pl>
2020-07-24 11:06:25 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# MCStart
+Automatically start a Minecraft server whenever a whitelisted player tries to join.
---
+++ src/main/kotlin/pl/szczodrzynski/mcstart/Main.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Kuba Szczodrzyński 2020-7-24.
+ */
+
+package pl.szczodrzynski.mcstart
+
+fun main(args: Array<String>) {
+
+}
