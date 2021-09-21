558a0f4ad6be5807759b49e923486dea221ae341
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-09-21 21:31:57 +0200

    Change max heap size to 256MB

--- docker-install.sh
+++ docker-install.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/kuba2k2/mcstart/releases/download/v1.1.0/mcstart-1.1.0.jar -O /mcstart.jar
 sed -i 's/exec /mcstart /g' /start-finalExec
-echo "function mcstart(){ exec java -Xmx128M -jar /mcstart.jar \$* ; }" >> /start-utils
+echo "function mcstart(){ exec java -Xmx256M -jar /mcstart.jar \$* ; }" >> /start-utils
