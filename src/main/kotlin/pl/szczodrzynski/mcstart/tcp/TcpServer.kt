2fd2bbf33d844c9755f81d4ac8fdf0aa040376ec
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-09-21 13:03:33 +0200

    Add stdin commands for TcpServer

--- src/main/kotlin/pl/szczodrzynski/mcstart/tcp/TcpServer.kt
+++ src/main/kotlin/pl/szczodrzynski/mcstart/tcp/TcpServer.kt
@@ -4,32 +4,54 @@
 
 package pl.szczodrzynski.mcstart.tcp
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import pl.szczodrzynski.mcstart.config.Config
 import pl.szczodrzynski.mcstart.ext.log
 import java.net.ServerSocket
 
 class TcpServer(
     config: Config,
-) {
+) : CoroutineScope {
 
+    override val coroutineContext = Job() + Dispatchers.IO
     private val server = ServerSocket(config.serverPort)
+    private var scannerJob: Job? = null
 
     init {
         println("----")
         log("Server running on port ${server.localPort}")
+
+        scannerJob = launch(Dispatchers.IO) {
+            while (!server.isClosed) {
+                val command = readLine()
+                if (command.isNullOrBlank())
+                    continue
+                when (command.lowercase()) {
+                    "start" -> onServerClose("CONSOLE")
+                    "exit", "stop" -> Runtime.getRuntime().exit(0)
+                    else -> println("Unknown command: $command")
+                }
+            }
+        }
 
         while (!server.isClosed) {
             try {
                 val client = server.accept()
                 ClientHandlerThread(config, client, this::onServerClose)
             } catch (e: Exception) {
-                log("!!! The server threw an Exception: $e")
+                if (e.message?.contains("accept failed") != true && e.message?.contains("Socket closed") != true)
+                    log("!!! The server threw an Exception: $e")
             }
         }
+        scannerJob?.cancel()
     }
 
     private fun onServerClose(username: String) {
         log("Server started by '$username', shutting down TCP listener")
         server.close()
+        scannerJob?.cancel()
     }
 }
