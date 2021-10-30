3f089baae8e68c0e5bc0f874b88eb231bbb57ddc
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-10-30 15:56:46 +0200

    Fix bash syntax error in start-utils

--- docker-install.sh
+++ docker-install.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/kuba2k2/mcstart/releases/download/v1.2.1/mcstart-1.2.1.jar -O /mcstart.jar
 sed -i 's/exec /mcstart /g' /start-finalExec
-echo "function mcstart(){ exec java -Xmx256M -jar /mcstart.jar \$* ; }" >> /start-utils
+echo -e "\n\nfunction mcstart(){ exec java -Xmx256M -jar /mcstart.jar \$* ; }" >> /start-utils
