b2f214197fbc08dbc82a269eaf62d01f226dffc3
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-10-30 21:13:38 +0200

    Add uploading GitHub release asset

--- docker-install.sh
+++ docker-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 wget https://github.com/kuba2k2/mcstart/releases/download/v1.2.1/mcstart-1.2.1.jar -O /mcstart.jar
 sed -i 's/exec /mcstart /g' /start-finalExec
 echo -e "\n\nfunction mcstart(){ exec java -Xmx256M -jar /mcstart.jar \$* ; }" >> /start-utils
